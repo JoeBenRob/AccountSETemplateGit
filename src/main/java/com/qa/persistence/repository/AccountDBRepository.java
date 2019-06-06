a1c7a4a16f0016e02d9463c50f00fa11a216a9b2
JoeBenRob <joebenrob@gmail.com>
2019-06-06 12:31:26 +0100

    Fix

---
+++ src/main/java/com/qa/persistence/repository/AccountDBRepository.java
@@ -0,0 +1,75 @@
+package com.qa.persistence.repository;
+
+import java.util.Collection;
+
+import javax.enterprise.inject.Default;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+import com.qa.persistence.domain.Account;
+import com.qa.util.JSONUtil;
+
+@Default
+@Transactional(TxType.SUPPORTS)
+public class AccountDBRepository implements AccountRepository {
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager em;
+	private JSONUtil json;
+
+	public String getAllAccounts() {
+
+		Query query = em.createQuery("SELECT a FROM Account a");
+		Collection<Account> accounts = (Collection<Account>) query.getResultList();
+		return json.getJSONForObject(accounts);
+
+	}
+
+	@Transactional(TxType.REQUIRED)
+	public String findAnAccount(int accountNumber) {
+		em.getTransaction().begin();
+		String acc = json.getJSONForObject(em.find(Account.class, accountNumber));
+		em.getTransaction().commit();
+		return acc;
+	}
+
+	@Override
+	@Transactional(TxType.REQUIRED)
+	public String createAccount(String account) {
+
+		em.getTransaction().begin();
+		Account acc = json.getObjectForJSON(account, Account.class);
+		em.persist(acc);
+		em.getTransaction().commit();
+		return "{\"message\": \"New Account Created\"}";
+	}
+
+	@Override
+	@Transactional(TxType.REQUIRED)
+	public String deleteAccount(int accountNumber) {
+		em.getTransaction().begin();
+		em.remove(accountNumber);
+		em.getTransaction().commit();
+		return "{\"message\": \"New Account Deleted\"}";
+	}
+
+	@Override
+	@Transactional(TxType.REQUIRED)
+	public String updateAccount(int accountNumber, String account) {
+
+		Account oldAcc = em.find(Account.class, accountNumber);
+		Account newAcc = json.getObjectForJSON(account, Account.class);
+
+		em.getTransaction().begin();
+		em.detach(oldAcc);
+		oldAcc = newAcc;
+		em.merge(oldAcc);
+		em.getTransaction().commit();
+
+		return "{\"message\": \"Account details updated\"}";
+	}
+
+}
--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -3,7 +3,6 @@ package com.qa.MapTests;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.qa.persistence.domain.Account;
@@ -12,122 +11,119 @@ import com.qa.util.JSONUtil;
 
 public class AccountServiceTest {
 
+	private JSONUtil jsonUtil;
 	private AccountMapRepository amr;
-	private Account acc1 = new Account(1, "D0001", "Matt", "Hunt");
-	private Account acc2 = new Account(2, "D0002", "Bert", "Smith");
-	private Account acc3 = new Account(3, "D0003", "Matt", "Hunt");
-	private JSONUtil jsonUtil = new JSONUtil();
+	private Account acc1;
+	private Account acc2;
+	private String acc1JSON = "{\"id\":1,\"accountNumber\":1234,\"firstName\":\"John\",\"lastName\":\"Smith\"}";
+	private String acc2JSON = "{\"id\":2,\"accountNumber\":12345,\"firstName\":\"Jane\",\"lastName\":\"Doe\"}";
 
 	@Before
 	public void setup() {
+		jsonUtil = new JSONUtil();
 		amr = new AccountMapRepository();
-	}
-
-	@Test
-	public void getAllAccountsTest() {
-
-		assertEquals("{}", amr.getAllAccounts());
-	}
-
-	@Test
-	public void getAllAccountsTest2() {
-
-		amr.getAccountMap().put(1, acc1);
-		assertEquals("{\"1\":{\"id\":1,\"accountNumber\":\"D0001\",\"firstName\":\"Matt\",\"lastName\":\"Hunt\"}}",
-				amr.getAllAccounts());
-	}
-
-	@Test
-	public void updateAccountTest() {
-
-		amr.getAccountMap().put(1, acc1);
-		amr.updateAccount(1, "{\"id\":1,\"accountNumber\":\"D0001\",\"firstName\":\"Matt\",\"lastName\":\"Wright\"}");
-		assertEquals("Wright", amr.getAccountMap().get(1).getLastName());
+		acc1 = new Account(1, 1234, "John", "Smith");
+		acc2 = new Account(2, 12345, "Jane", "Doe");
 	}
 
 	@Test
 	public void addAccountTest() {
-		String accToCreate = jsonUtil.getJSONForObject(acc1);
-		System.out.println(accToCreate);
-		assertEquals(amr.createAccount(accToCreate), "Account successfuly created");
-		assertEquals(amr.getAccountMap().size(), 1);
+
+		amr.createAccount(acc1JSON);
+
+		assertEquals(1, amr.getAccountMap().size());
+		assertEquals("John", amr.getAccountMap().get(1234).getFirstName());
 
 	}
 
 	@Test
 	public void add2AccountsTest() {
-		assertEquals(amr.createAccount(jsonUtil.getJSONForObject(acc1)), "Account successfuly created");
-		assertEquals(amr.createAccount(jsonUtil.getJSONForObject(acc2)), "Account successfuly created");
-		assertEquals(amr.getAccountMap().size(), 2);
+
+		amr.createAccount(acc1JSON);
+		amr.createAccount(acc2JSON);
+
+		assertEquals(2, amr.getAccountMap().size());
+		assertEquals("John", amr.getAccountMap().get(1234).getFirstName());
+		assertEquals("Jane", amr.getAccountMap().get(12345).getFirstName());
 	}
 
 	@Test
 	public void removeAccountTest() {
 
-		amr.getAccountMap().put(1, acc1);
+		amr.getAccountMap().put(1234, acc1);
 
-		amr.deleteAccount(1);
-		assertEquals(false, amr.getAccountMap().containsKey(1));
+		amr.deleteAccount(1234);
 
+		assertEquals(0, amr.getAccountMap().size());
 	}
 
 	@Test
 	public void remove2AccountsTest() {
-		amr.getAccountMap().put(1, acc1);
-		amr.getAccountMap().put(2, acc2);
 
-		amr.deleteAccount(1);
-		assertEquals(false, amr.getAccountMap().containsKey((Integer) 1));
+		amr.getAccountMap().put(1234, acc1);
+		amr.getAccountMap().put(12345, acc2);
+		amr.deleteAccount(1234);
+		amr.deleteAccount(12345);
 
-		amr.deleteAccount(2);
-		assertEquals(false, amr.getAccountMap().containsKey((Integer) 2));
-
+		assertEquals(0, amr.getAccountMap().size());
 	}
 
 	@Test
 	public void remove2AccountTestAnd1ThatDoesntExist() {
-		amr.getAccountMap().put(1, acc1);
-		amr.getAccountMap().put(2, acc2);
+		amr.getAccountMap().put(1234, acc1);
+		amr.getAccountMap().put(12345, acc2);
 
+		amr.deleteAccount(1234);
 		amr.deleteAccount(1);
-		assertEquals(false, amr.getAccountMap().containsKey((Integer) 1));
 
-		amr.deleteAccount(3);
-		assertEquals(false, amr.getAccountMap().containsKey((Integer) 3));
+		assertEquals(1, amr.getAccountMap().size());
+		assertEquals("Jane", amr.getAccountMap().get(12345).getFirstName());
 
 	}
 
 	@Test
 	public void jsonStringToAccountConversionTest() {
-		String stringToTest = "{\"id\":1,\"accountNumber\":\"D0001\",\"firstName\":\"Matt\",\"lastName\":\"Hunt\"}";
-		Account testAccount = jsonUtil.getObjectForJSON(stringToTest, Account.class);
-		assertEquals(acc1.getAccountNumber(), testAccount.getAccountNumber());
+		Account acc = jsonUtil.getObjectForJSON(acc1JSON, Account.class);
+		assertEquals(1234, acc.getAccountNumber());
 	}
 
 	@Test
 	public void accountConversionToJSONTest() {
-		String jsonAccount = "{\"id\":1,\"accountNumber\":\"D0001\",\"firstName\":\"Matt\",\"lastName\":\"Hunt\"}";
-		String stringToTest = jsonUtil.getJSONForObject(acc1);
-		assertEquals(jsonAccount, stringToTest);
-
+		assertEquals(acc1JSON, jsonUtil.getJSONForObject(acc1));
 	}
 
 	@Test
-	@Ignore
+	public void returnAccountsWhenMapEmpty() {
+		assertEquals("{}", amr.getAllAccounts());
+	}
+
+	@Test
+	public void returnAccountsWhenMapFilled() {
+		amr.getAccountMap().put(1234, acc1);
+		assertEquals(1, amr.getAccountMap().size());
+		assertEquals("{\"1234\":{\"id\":1,\"accountNumber\":1234,\"firstName\":\"John\",\"lastName\":\"Smith\"}}",
+				amr.getAllAccounts());
+	}
+
+	@Test
 	public void getCountForFirstNamesInAccountWhenZeroOccurances() {
-
+		assertEquals(0, amr.findAccountsWithFirstName("John"));
 	}
 
 	@Test
-	@Ignore
 	public void getCountForFirstNamesInAccountWhenOne() {
+		amr.getAccountMap().put(1234, acc1);
 
+		assertEquals(1, amr.findAccountsWithFirstName("John"));
 	}
 
 	@Test
-	@Ignore
 	public void getCountForFirstNamesInAccountWhenTwo() {
+		Account acc3 = new Account(3, 12, "John", "Doe");
+		amr.getAccountMap().put(1234, acc1);
+		amr.getAccountMap().put(12, acc3);
 
+		assertEquals(2, amr.findAccountsWithFirstName("John"));
 	}
 
 }
